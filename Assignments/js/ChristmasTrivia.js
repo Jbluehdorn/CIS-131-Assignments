ed1669a4add91169675eb62fd3147b51498b9c20
Jordan Bluehdorn <Jordan Bluehdorn>
2015-12-10 21:19:30 -0600

    final project finished

--- Assignments/js/ChristmasTrivia.js
+++ Assignments/js/ChristmasTrivia.js
@@ -1,9 +1,15 @@
 // JavaScript Document
 var Categories = new Array();
 GetTrivia();
+
+//Holds the display image
 var DispImage = new Image();
 DispImage.src = "images/christmas/default.png";
 DispImage.id = "TriviaImage";
+
+var totalCorrect = 0;
+var totalQuestions = 0;
+var questionAnswered = false;
 
 $(document).ready(function() {
 	SetStartScreen();
@@ -14,7 +20,7 @@ $(document).ready(function() {
 function GetTrivia() {
 	$.getJSON('json/Questions.json', function(data) {
 		for(var i = 0; i < data.categories.length; i++) {
-			Categories.push(new Category(data.categories[i].id, data.categories[i].name, data.categories[i].image));
+			Categories.push(new Category(data.categories[i].id, data.categories[i].name, data.categories[i].image, data.categories[i].description));
 		}
 		for(var i = 0; i < data.questions.length; i++) {
 			var question = new Question(data.questions[i].question, data.questions[i].answers, data.questions[i].categoryId);
@@ -32,12 +38,29 @@ function GetTrivia() {
 function SetStartScreen() {
 	var board = $('#TriviaBoard');
 	var menu = CreateMenu();
-	board.append(menu);
-	board.append(DispImage);
+	var results = GetResults();
+	
+	var errorMessage = $('<div id="ErrorMessage">');
+	errorMessage.append('*Please select an answer');
+	
+	board
+		.append(menu)
+		.append(DispImage)
+		.append(errorMessage)
+		.append(results);
+}
+
+function GetResults() {
+	var results = $('<div id="Results">');
+	results.append('Correct: ' + totalCorrect + '/' + totalQuestions);	
+	return results;
 }
 
 //Builds the menu based on the array
 function CreateMenu() {
+	var menuHolder = $('<div id="menuDiv">');
+	var menuHeader = $('<h5 id="menuHeader">');
+	menuHeader.html('Select a category...');
 	var menu = $('<ul>');
 	for(var i = 0; i < Categories.length; i++){
 		var element = $('<li>');
@@ -52,7 +75,10 @@ function CreateMenu() {
 		});
 		$(menu).append(element);
 	}
-	return $(menu);
+	menuHolder
+		.append(menu)
+		.append(menuHeader);
+	return $(menuHolder);
 }
 
 //Loads the selcted trivia game
@@ -63,6 +89,8 @@ function LoadTrivia(selected) {
 			Trivia = Categories[i];	
 		}
 	}
+	
+	$('#menuHeader').html(Trivia.description);
 	$('#TriviaImage').replaceWith(Trivia.image);
 	$('#TriviaBoard').append(CreateRandomQuestion(Trivia.questions));
 }
@@ -74,6 +102,8 @@ function LoadTrivia(selected) {
 // 5. Returns the div
 function CreateRandomQuestion(questions) {
 	$('#questionDiv').remove();
+	$('#ErrorMessage').hide();
+	questionAnswered = false;
 	var questionDiv = $('<div id="questionDiv">');
 	
 	var num = Math.floor(Math.random() * questions.length);
@@ -83,8 +113,10 @@ function CreateRandomQuestion(questions) {
 	
 	var answers = new Array();
 	var correctAnswer;
+	//Randomizes the answers
 	question.answers = shuffleArray(question.answers);
-	console.log(questions.answers);
+	
+	//Builds objects based on the answers
 	for(var i = 0; i < question.answers.length; i++) {
 		if(question.answers[i] === question.correct) 
 			correctAnswer = question.answers[i];
@@ -92,11 +124,13 @@ function CreateRandomQuestion(questions) {
 		var element = $('<div class="answer roundedCorners">');
 		$(element).html(question.answers[i]);
 		$(element).on('click', function() {
-			var answerObjs = $('.answer');
-			for(var i = 0; i < answerObjs.length; i++) {
-				$(answerObjs[i]).removeAttr('selected');
+			if(!questionAnswered) {
+				var answerObjs = $('.answer');
+				for(var i = 0; i < answerObjs.length; i++) {
+					$(answerObjs[i]).removeAttr('selected');
+				}
+				$(this).attr('selected', 'true');
 			}
-			$(this).attr('selected', 'true');
 		});
 		answers.push(element);
 	}
@@ -110,7 +144,8 @@ function CreateRandomQuestion(questions) {
 	
 	var btnSubmit = $('<div class="btn btn-primary">');
 	$(btnSubmit).on('click', function() {
-		CheckAnswer(correctAnswer);
+		if(!questionAnswered)
+			CheckAnswer(correctAnswer);
 	});
 	$(btnSubmit).append('Submit');
 	
@@ -134,11 +169,36 @@ function CreateRandomQuestion(questions) {
 	return questionDiv;
 }
 
-function CheckAnswer(correct) {
-	var selected = $('.answer[selected]').html();
-	alert(selected === correct ? 'yup' : 'nope');
+//Checks if the answer is correct and updates fields accordingly
+function CheckAnswer(CorrectAnswer) {
+	var selected = $('.answer[selected]');
+	if(!selected.length) {
+		$('#ErrorMessage').show();
+		return;
+	}
+	$('#ErrorMessage').hide();
+			
+	questionAnswered = true;
+	totalQuestions++;
+	var correct = selected.html() == CorrectAnswer;
+	var answers = $('.answer');
+	
+	for(var i = 0; i < answers.length; i++) {
+		if($(answers[i]).html() !== CorrectAnswer)
+			$(answers[i]).fadeTo(400, 0.3);
+	}
+	
+	if(correct) {
+		totalCorrect++;
+	} else {
+		selected.attr('incorrect', 'true');
+	}
+	
+	$('#Results').html(GetResults().html());
+	
 }
 
+//Randomizes the array
 function shuffleArray(array) {
     for (var i = array.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
@@ -150,13 +210,14 @@ function shuffleArray(array) {
 }
 
 /*==Constructors==*/
-function Category(id, name, image) {
+function Category(id, name, image, description) {
 	this.questions = new Array();
 	this.id = id;
 	this.name = name;
 	this.image = new Image();
 	this.image.src = image;
 	this.image.id = 'TriviaImage';
+	this.description = description;
 }
 
 function Question(question, answers, categoryId) {
